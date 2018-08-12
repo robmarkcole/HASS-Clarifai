ed0fa9d0b5f346482284e195e5bd6544d4808bd5
robmarkcole <robmarkcole@gmail.com>
2018-08-12 09:56:05 +0100

    Initial

--- custom_components/image_processing/clarifai.py
+++ custom_components/image_processing/clarifai.py
@@ -18,16 +18,14 @@ from homeassistant.components.image_processing import (
 
 _LOGGER = logging.getLogger(__name__)
 
-SCAN_INTERVAL = timedelta(seconds=20)
-
 CONF_API_KEY = 'api_key'
-CONF_MODEL_ID = 'model_id'
 CONF_CONCEPTS = 'concepts'
 DEFAULT_CONCEPTS = 'NO_CONCEPT'
 
+REQUIREMENTS = ['clarifai==2.2.3']
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_API_KEY): cv.string,
-    vol.Required(CONF_MODEL_ID): cv.string,
     vol.Optional(CONF_CONCEPTS, default=[DEFAULT_CONCEPTS]):
         vol.All(cv.ensure_list, [cv.string]),
 })
@@ -38,10 +36,8 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     entities = []
     for camera in config[CONF_SOURCE]:
         entities.append(ClarifaiClassifier(
-            hass,
             camera.get(CONF_NAME),
             config[CONF_API_KEY],
-            config[CONF_MODEL_ID],
             config[CONF_CONCEPTS],
             camera[CONF_ENTITY_ID],
         ))
@@ -53,9 +49,12 @@ class ClarifaiClassifier(ImageProcessingEntity):
 
     ICON = 'mdi:file'
 
-    def __init__(self, hass, name, API_key, model_id, concepts, camera_entity):
-        """Init with the API key and model id"""
-        self.hass = hass
+    def __init__(self, name, API_key, concepts, camera_entity):
+        """Init with the API key."""
+        from clarifai.rest import ClarifaiApp
+        self.app = ClarifaiApp(api_key=API_key)
+        self.model_name = 'general-v1.3'
+        self.model = self.app.models.get(self.model_name)
         if name:  # Since name is optional.
             self._name = name
         else:
@@ -63,23 +62,13 @@ class ClarifaiClassifier(ImageProcessingEntity):
                 split_entity_id(camera_entity)[1])
         self._concepts = concepts
         self._camera_entity = camera_entity
-        self._headers = {
-            'content-type': 'application/json',
-            'Authorization': 'Key ' + API_key
-        }
-        self._url = "https://api.clarifai.com/v2/models/{}/outputs".format(
-            model_id)
         self._classifications = {}  # The dict of classifications
         self._state = None    # The most likely classification
 
     def process_image(self, image):
         """Perform classification of a single image."""
-        base64_img = base64.b64encode(image).decode('ascii')
-        json_data = {
-            "inputs": [{"data": {"image": {"base64": base64_img}}}]
-        }
-        response = requests.post(
-            self._url, headers=self._headers, json=json_data).json()
+        base64_img = base64.b64encode(image)
+        response = self.model.predict_by_base64(base64_img)
 
         if response['status']['description'] == 'Ok':
             data = response['outputs'][0]['data']['concepts']
